34761b9b723c7b45f1d2e143ef9db97d0b6888ca
Jose-Manuel-Gonz <71922222+Jose-Manuel-Gonz@users.noreply.github.com>
2020-10-23 03:39:47 -0500

    Add files via upload

---
+++ PREGUNTAS_3.2_3.6.c
@@ -0,0 +1,72 @@
+// 3. PREGUNTAS
+
+//		2. ESCRIBA LAS DECLARACIONES APROPIADAS PARA CADA UNA DE LAS VARIABLES
+		   
+//			*VARIABLES ENTERAS: p,q
+				int p, q;
+
+//			*VARIABLES DE TIPO FLOTANTE: x,y,z
+				float x, y, z;	
+			
+//			*VARIABLES DE TIPO CARACTER: a,b,c
+				char a, b, c;
+			
+
+//		3. ESCRIBA LAS DECLARACIONES APROPIADAS PARA CADA UNA DE LAS SIGUIENTES VARIABLES
+
+//			*VARIABLES DE PUNTO FLOTANTE: root1,root2
+				float root1, root2;
+			
+//			*VARIABLES PARA UN ENTERO LARGO: counter
+				long counter;
+			
+//			*VARIABLE DE ENTERA CORTA: flag
+				short flag;
+			
+			
+//		4. ESCRIBA LAS DECLARACIONES APROPIADAS PARA CADA UNA DE LAS SIGUIENTES VARIABLES
+
+//			*VARIABLE ENTERA: index
+				int	index;
+				
+//			*VARIABLE ENTERA SIN SIGNO: cust_no
+				unsigned int cust_no;
+				
+//			*VARIABLE DE DOBLE PRECISION: gros, tax, net 
+				double gros, tax, net;
+				
+//			*VARIABLE DE TIPO PUNTO FLOTANTE: error
+				float error;
+		
+		
+//		5. ESCRIBA LAS DECLARACIONES E INICIALIZACIONES DE VARIABLES,
+//			CONFORME SE MUESTRA A CONTINUACION
+
+//			*VARIABLES DE PUNTO FLOTANTE: a=-8.2 y b=0.005
+				float a = -8.2, b = 0.005;
+				
+//			*VARIABLES DE TIPO ENTERO: x=129, y=87 y z=-22
+				int x = 129, y = 87, z = -22;
+				
+//			*VARIABLES DE TIPO CARACTER: c1='w', c2='&'
+				char c1 = 'w', c2 = '&';
+				
+
+//		6. EXPLIQUE EL OBJETIVO DE CADA EXPRESION
+			
+//			* a - b: Resta la variable "b" a la variable "a"
+
+//			* a * (b + c): Suma "b" y "c" y ese resultado
+//						   lo multiplica por "a"
+
+//			* d = a * (b + c): La suma de "b" y "c" multiplicada
+//						       por "a" lo guarda en la variable "d"
+
+//			* a >= b: Esta expresion afirma que "a" es mayor o igual
+//					  que "b"
+
+//			* (a % 5) == 0: Esta expersion nos dice que el residuo de la 
+//							division de la variable "a" entre 5 es 0
+
+
+		
